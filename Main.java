71d95f86f8d6afb3cb9f9463e80ec135f2d0de24
Aneetta Thomas <96110446+aneettats@users.noreply.github.com>
2023-01-25 15:12:34 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+
+        int i=1;
+        System.out.println("Welcome to the program");
+
+        while(i<=10)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
